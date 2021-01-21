b5f6842c4a956865d212a4395b429f3636e32247
Silvia Criado <silvie.criado@gmail.com>
2021-01-21 14:38:03 +0100

    "M1-Fase1 completed"

---
+++ src/M1/fase1/Date.java
@@ -0,0 +1,10 @@
+package M1.fase1;
+
+public class Date {
+    public static void main(String[] args) {
+        int day = 15;
+        int month = 1;
+        int year = 2021;
+        System.out.println(day + "/" + month + "/" + year);
+    }
+}
---
+++ src/M1/fase1/fullName.java
@@ -0,0 +1,10 @@
+package M1.fase1;
+
+public class fullName {
+    public static void main(String[] args) {
+        String name = "Silvia";
+        String surname1 = "Criado";
+        String surname2 ="Sañudo";
+        System.out.println(surname1 + " " + surname2 + " " + name);
+    }
+}
