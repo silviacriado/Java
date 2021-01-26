11b90645576689c9c7173426e72227553c4775df
Silvia Criado <silvie.criado@gmail.com>
2021-01-27 00:32:53 +0100

    updated fase2 exercise

--- src/M1/fase2/isLeapYear.java
+++ src/M1/fase2/isLeapYear.java
@@ -2,19 +2,21 @@ package M1.fase2;
 
 public class isLeapYear {
     public static void main(String[] args) {
-        
+
         // Sabent que l’any 1948 es un any de traspàs:
         // Creeu una constant amb el valor de l’any (1948)
 
         final int year;
-        year = 1948;
+        year = 1952;
 
-        if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
-            System.out.println(year + " es un año bisiesto");
-        } else {
-            System.out.println(year + " no es un año bisiesto");
+        // Creeu una variable que guardi cada quan hi ha un any de traspàs
+
+        int leapYear = (year + 4);
+            if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println((year + 4));
         }
     }
 }
+
 
 
---
+++ src/M1/fase4/personalDetails.java
@@ -0,0 +1,10 @@
+package M1.fase4;
+
+public class personalDetails {
+    public static void main(String[] args) {
+      String [] name = {"Silvia"," ", "Criado", " ", "Sañudo"};
+        for (String n : name) {
+            System.out.print(n);
+        }
+    }
+}
