71e18db1b95f7ee7e692dbd033c349c008c55766
Silvia Criado <silvie.criado@gmail.com>
2021-01-31 18:43:34 +0100

    M1 nivel2 completed

--- src/M1/nivel2/cast.java
+++ src/M1/nivel2/cast.java
@@ -2,7 +2,15 @@ package M1.nivel2;
 
 public class cast {
     public static void main(String[] args) {
-        double prix = 124.5;
-        System.out.println(prix);
+        double d = 3.1415;
+        int i = (int) d;
+        System.out.println(d);
+        System.out.println(i);
+
+        float f = (float) d;
+        System.out.println(f);
+
+        String s = String.valueOf(d);
+        System.out.println(s);
     }
 }
