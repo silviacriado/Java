86a7245e764fd01c387823d5942a657c5aaf0ab7
Silvia Criado <silvie.criado@gmail.com>
2021-01-31 16:17:45 +0100

    fase4 completed

---
+++ src/M1/fase4/myPersonalDetails.java
@@ -0,0 +1,20 @@
+package M1.fase4;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class myPersonalDetails {
+    public static void main(String[] args) {
+        String fullName = ("Silvia" + " " + "Criado" + " " + "Sañudo");
+        System.out.println("Mi nombre es " + fullName);
+
+        Date birthDate = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat ("08/06/1978");
+        System.out.println("Nací el: " + ft.format(birthDate));
+
+        int year = 1980;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+                    System.out.println("Mi año de nacimiento es un año bisiesto");
+                } else System.out.println("Mi año de nacimiento no es un año bisiesto");
+    }
+}
