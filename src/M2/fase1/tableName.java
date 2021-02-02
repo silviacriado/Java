ced3645e62d50e6be2b5d48259ddc4f7e5207e66
Silvia Criado <silvie.criado@gmail.com>
2021-02-02 14:20:01 +0100

    M2 created

---
+++ src/M2/fase1/tableName.java
@@ -0,0 +1,11 @@
+package M2.fase1;
+
+public class tableName {
+    public static void main(String[] args) {
+        String[] name = new String[]{"S", "i", "l", "v", "i", "a"};
+        String fullName = "Silvia";
+        for (int i = 0; i < name.length; i++) {
+            System.out.print(fullName.charAt(i));
+        }
+    }
+}
