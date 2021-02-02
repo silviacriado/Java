cabee8822ea2dbdb867e95c521943135b499165c
Silvia Criado <silvie.criado@gmail.com>
2021-02-02 23:05:08 +0100

    M2 fase1 completed

--- src/M2/fase1/charName.java
+++ src/M2/fase1/charName.java
@@ -2,10 +2,10 @@ package M2.fase1;
 
 public class charName {
     public static void main(String[] args) {
-        char name[] = {'S','i','l','v','i','a'};
-        String fullName = new String(name);
-        for (int i = 0; i < name.length; i++) {
-            System.out.print(fullName.charAt(i));
+        char[] letter = {'S','i','l','v','i','a'};
+        String name = new String(letter);
+        for (int i = 0; i < letter.length; i++) {
+            System.out.print(name.charAt(i));
         }
     }
 }
