ad309c52983df9ef9cf8a9528502e77ac7d38f01
Silvia Criado <silvie.criado@gmail.com>
2021-02-03 15:13:05 +0100

    M2 fase2 chaged table to list character

--- src/M2/fase2/ListName.java
+++ src/M2/fase2/ListName.java
@@ -1,21 +1,20 @@
 package M2.fase2;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ListName {
     public static void main(String[] args) {
         char[] letter = {'S','i','l','v','i','a'};
-        String name = new String(letter);
-        for (int i = 0; i < letter.length; i++) {
-            System.out.print(name.charAt(i));
-        }
-        // change String to Array for then tp change into list
-        char[] chars = name.toCharArray();
-        List<Character> listC = new ArrayList<>();
-        for (char c : chars) {
-            listC.add(c);
-        }
-        System.out.println(listC);
+        Stream<Character> myStreamOfCharacters = IntStream
+                .range(0, letter.length)
+                .mapToObj(i -> letter[i]);
+
+        List<Character> myListOfCharacters = myStreamOfCharacters.collect(Collectors.toList());
+        myListOfCharacters.forEach(System.out::println);
+
+        
     }
 }
