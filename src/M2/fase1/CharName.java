84bb996bffd3800eb572f5495c88b36915321b2a
Silvia Criado <silvie.criado@gmail.com>
2021-02-04 02:23:48 +0100

    updated .idea name project

--- src/M2/fase1/CharName.java
+++ src/M2/fase1/CharName.java
@@ -2,9 +2,10 @@ package M2.fase1;
 
 public class CharName {
     public static void main(String[] args) {
-        char[] letter = {'S','i','l','v','i','a'};
-        String name = new String(letter);
-        for (int i = 0; i < letter.length; i++) {
+        char[] cName = {'S','i','l','v','i','a'};
+        String name = new String(cName);
+        //loop to store each character in the array to print
+        for (int i = 0; i < cName.length; i++) {
             System.out.print(name.charAt(i));
         }
     }
--- src/M2/fase2/ListName.java
+++ src/M2/fase2/ListName.java
@@ -1,20 +1,26 @@
 package M2.fase2;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.ArrayList;
 
 public class ListName {
     public static void main(String[] args) {
-        char[] letter = {'S','i','l','v','i','a'};
-        Stream<Character> myStreamOfCharacters = IntStream
-                .range(0, letter.length)
-                .mapToObj(i -> letter[i]);
-
-        List<Character> myListOfCharacters = myStreamOfCharacters.collect(Collectors.toList());
-        myListOfCharacters.forEach(System.out::println);
-
-        
+        char[] c = {'S', 'i', 'l', 'v', 'i', 'a'};
+        ArrayList <Character> list = new ArrayList<Character>();
+        list.add('S');
+        list.add('i');
+        list.add('l');
+        list.add('v');
+        list.add('i');
+        list.add('a');
+        for (int i = 0; i < c.length; i++){
+            if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') ||
+                (c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')) {
+            System.out.println("VOCAL");
+        } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println("CONSONANTE");
+        else
+            System.out.println("");
+            i++;
+        }
     }
 }
