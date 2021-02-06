2112896fc6b752f2a3f2c3499c7c3e4741ebbf3d
Silvia Criado <silvie.criado@gmail.com>
2021-02-06 01:17:27 +0100

    M2 Fase2 exercise completed

--- src/M2/fase2/ListName.java
+++ src/M2/fase2/ListName.java
@@ -4,23 +4,19 @@ import java.util.ArrayList;
 
 public class ListName {
     public static void main(String[] args) {
-        char[] c = {'S', 'i', 'l', 'v', 'i', 'a'};
-        ArrayList <Character> list = new ArrayList<Character>();
-        list.add('S');
-        list.add('i');
-        list.add('l');
-        list.add('v');
-        list.add('i');
-        list.add('a');
-        for (int i = 0; i < c.length; i++){
+        char[] cName = {'S', 'i', 'l', 'v', 'i', 'a'};
+        // Covert char Array into String Array to change it to List
+        ArrayList<Character> listComplete = new ArrayList<>();
+        for (char c : cName) {
+            listComplete.add(c);
             if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') ||
-                (c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')) {
-            System.out.println("VOCAL");
-        } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-            System.out.println("CONSONANTE");
-        else
-            System.out.println("");
-            i++;
+                    (c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')) {
+                System.out.println("VOCAL");
+            } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+                System.out.println("CONSONANTE");
+            } else
+                System.out.println("");{
+            }
         }
     }
 }
