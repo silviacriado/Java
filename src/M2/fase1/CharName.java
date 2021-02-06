587d4ed931fc95451ad7913962e729466c56353c
Silvia Criado <silvie.criado@gmail.com>
2021-02-06 01:11:50 +0100

    M2 fase1 exercise completed

--- src/M2/fase1/CharName.java
+++ src/M2/fase1/CharName.java
@@ -2,11 +2,9 @@ package M2.fase1;
 
 public class CharName {
     public static void main(String[] args) {
-        char[] cName = {'S','i','l','v','i','a'};
-        String name = new String(cName);
-        //loop to store each character in the array to print
-        for (int i = 0; i < cName.length; i++) {
-            System.out.print(name.charAt(i));
+        char[] cName = {'S', 'i', 'l', 'v', 'i', 'a'};
+        for (char c : cName) {
+            System.out.print(c);
         }
     }
 }
