1878c930f099c58fc7f046251b987316e979358a
Silvia Criado <silvie.criado@gmail.com>
2021-02-06 16:17:32 +0100

    M2 fase4 completed

---
+++ src/M2/fase4/SurnameList.java
@@ -0,0 +1,14 @@
+package M2.fase4;
+
+import java.util.ArrayList;
+
+public class SurnameList {
+    public static void main(String[] args) {
+        String[] cSurname = {"Criado"};
+        // Covert char String Array into StringList
+        ArrayList<String> listSurname = new ArrayList<String>();
+        listSurname.add("Criado");
+        System.out.println(listSurname);
+    }
+}
+
