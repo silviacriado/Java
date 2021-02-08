c949e08fd45012286cfa88b56b5caee2f74b9a21
Silvia Criado <silvie.criado@gmail.com>
2021-02-08 11:18:22 +0100

    M2 fase4 completed

--- src/M2/fase4/SurnameList.java
+++ src/M2/fase4/SurnameList.java
@@ -4,11 +4,28 @@ import java.util.ArrayList;
 
 public class SurnameList {
     public static void main(String[] args) {
-        String[] cSurname = {"Criado"};
-        // Covert char String Array into StringList
-        ArrayList<String> listSurname = new ArrayList<String>();
-        listSurname.add("Criado");
-        System.out.println(listSurname);
+        ArrayList<Character> surnameArray = new ArrayList<>();
+        surnameArray.add('C');
+        surnameArray.add('r');
+        surnameArray.add('i');
+        surnameArray.add('a');
+        surnameArray.add('d');
+        surnameArray.add('o');
+
+        // Arraylist nombre
+        ArrayList<Character> nameComplete = new ArrayList<>();
+        nameComplete.add('S');
+        nameComplete.add('i');
+        nameComplete.add('l');
+        nameComplete.add('v');
+        nameComplete.add('i');
+        nameComplete.add('a');
+
+        ArrayList<String> fullName = new ArrayList<>();
+        fullName.add(String.valueOf(nameComplete));
+        fullName.add(String.valueOf(surnameArray));
+        System.out.println("Fullname: " + fullName);
     }
 }
+
 
