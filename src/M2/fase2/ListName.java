4e5ba270e815369b527b5d95d42fca33371a841e
Silvia Criado <silvie.criado@gmail.com>
2021-02-08 11:16:39 +0100

    M2 fase2 completed

--- src/M2/fase2/ListName.java
+++ src/M2/fase2/ListName.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 
 public class ListName {
     public static void main(String[] args) {
-        char[] cName = {'S', 'i', 'l', 'v', 'i', 'a', 7};
+        char[] cName = {'S', 'i', 'l', 'v', 'i', 'a'};
         // Covert char Array into String Array to change it to List
-        ArrayList<Character> listComplete = new ArrayList<>();
+        ArrayList<Character> nameComplete = new ArrayList<>();
+        nameComplete.add('S');
+        nameComplete.add('i');
+        nameComplete.add('l');
+        nameComplete.add('v');
+        nameComplete.add('i');
+        nameComplete.add('a');
         for (char c : cName) {
-            listComplete.add(c);
+            nameComplete.add(c);
             if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') ||
                     (c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')) {
                 System.out.println("VOCAL");
--- src/M2/fase3/MapName.java
+++ src/M2/fase3/MapName.java
@@ -1,16 +1,17 @@
 package M2.fase3;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.TreeMap;
 
 public class MapArray {
     public static void main(String[] args) {
         char[] cName = {'S', 'i', 'l', 'v', 'i', 'a'};
-        // Covert char Array into String Array to change it to List
         for (char c : cName) {
             System.out.println(c);
         }
-        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<String, List<Integer>> map = new HashMap<String, List<Integer>>();
+        //HashMap<Integer, String> map = new HashMap<>();
         map.put(1, String.valueOf(cName));
         TreeMap<Integer, String> treeMap = new TreeMap<>(map);
         System.out.println(treeMap);
