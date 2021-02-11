9031dbef0583500f6aa7b174f35cf5b1062cb4fd
Silvia Criado <silvie.criado@gmail.com>
2021-02-11 01:26:05 +0100

    M2 fase3 exercise corrected

--- src/M2/fase3/nameHashMap.java
+++ src/M2/fase3/nameHashMap.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 public class nameHashMap {
     public static void main(String[] args) {
         //Emmagatzemar en un Map tant les lletres de la llista com el nombre de vegades que apareixen
-        //String name = "Silvia";
+        //char[] cName = {'S', 'i', 'l', 'v', 'i', 'a'};
         HashMap<Character, Integer> hashName = new HashMap<>();
-        hashName.put('S', 1);
+        hashName.put('s', 1);
         hashName.put('i', 2);
         hashName.put('l', 1);
         hashName.put('v', 1);
