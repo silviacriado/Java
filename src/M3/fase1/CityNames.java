0ab83d05fcea864cd7b36769eff6dda791afbec2
Silvia Criado <silvie.criado@gmail.com>
2021-02-15 01:02:07 +0100

    M3 fase1 completed

---
+++ src/M3/fase1/CityNames.java
@@ -0,0 +1,23 @@
+package M3.fase1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CityNames {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce el nombre de estas ciudades: Barcelona, Madrid, Valencia, Malaga, Cadiz y Santader: ");
+
+        ArrayList<String> city = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Nombre de la ciudad: ");
+            city.add(input.next());
+        }
+        for (String s : city) {
+            System.out.println(s);
+        }
+    }
+}
+
+
