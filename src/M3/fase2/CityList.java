0b6df4bb80bbe92261d36843eb559123796621fb
Silvia Criado <silvie.criado@gmail.com>
2021-02-16 00:20:46 +0100

    M3 fase2 completed

---
+++ src/M3/fase2/CityList.java
@@ -0,0 +1,22 @@
+package M3.fase2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CityList {
+    public static void main(String[] args) {
+        ArrayList<String> city = new ArrayList<>();
+
+        city.add("Barcelona");
+        city.add("Madrid");
+        city.add("Valencia");
+        city.add("Malaga");
+        city.add("Cadiz");
+        city.add("Santander");
+
+        Collections.sort(city);
+        for (String s : city) {
+            System.out.println(s);
+        }
+    }
+}
