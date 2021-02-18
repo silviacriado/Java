1bb464c8c1af43f553cbc693572b1e3fa35ba8fe
Silvia Criado <silvie.criado@gmail.com>
2021-02-18 12:41:01 +0100

    M3 fase1 completed

--- src/M3/fase1/EmptyCities.java
+++ src/M3/fase1/EmptyCities.java
@@ -2,12 +2,11 @@ package M3.fase1;
 
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
 public class EmptyCities {
     public static void main(String[] args) {
-        Scanner input = new Scanner(in);
-        out.println("Escribe el nombre de la ciudad: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Escribe el nombre de la ciudad: ");
+
         String city1 = input.next();
         String city2 = input.next();
         String city3 = input.next();
@@ -15,8 +14,7 @@ public class EmptyCities {
         String city5 = input.next();
         String city6 = input.next();
 
-        out.print(city1 + " " + city2 + " " + city3 + " " + city4 + " " + city5 + " " + city6);
-
+        System.out.print(city1 + " " + city2 + " " + city3 + " " + city4 + " " + city5 + " " + city6);
         input.close();
     }
 }
