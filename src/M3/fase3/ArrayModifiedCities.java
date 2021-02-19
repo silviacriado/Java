4937450b0205385d2d650b807c0608efd1dc490d
Silvia Criado <silvie.criado@gmail.com>
2021-02-19 08:31:41 +0100

    M3 fase3 completed

--- src/M3/fase3/ArrayModifiedCities.java
+++ src/M3/fase3/ArrayModifiedCities.java
@@ -5,11 +5,6 @@ public class ArrayModifiedCities {
 
         String cities = "Barcelona, Madrid, Valencia, Malaga, Cadiz, Santander";
         char charToReplace = 'a';
-
-        if (cities.indexOf(charToReplace) == -1) {
-            System.out.print("La letra no existe");
-        }
-
         int count = 4;
         for (int i = 0; i < cities.length(); i++) {
             char ch = cities.charAt(i);
