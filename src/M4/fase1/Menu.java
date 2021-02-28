6b99543f748f24e0c2c7a592fc587639f625785c
Silvia Criado <silvie.criado@gmail.com>
2021-02-28 08:02:43 +0100

    M4 fase1 completed

--- src/M4/fase1/Menu.java
+++ src/M4/fase1/Menu.java
@@ -1,51 +1,99 @@
 package M4.fase1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
 
-        //Creamos la primera variable: billetes de 5-500€
-        int[] currency = {5, 10, 20, 50, 100, 200, 500};
-        System.out.println("Original Array:\n" + Arrays.toString(currency));
-    }
+        //Variable billetes de 5-500€:
 
-    public static class MenuRestaurant {
+        int fiveEur = 5;
+        int tenEur = 10;
+        int twentyEur = 20;
+        int fiftyEur = 50;
+        int hundredEur = 100;
+        int twoHundredEur = 200;
+        int fiveHundredEur = 500;
 
-        // this instance variable is visible for any child class.
-        public String menu;
 
-        // price  variable is visible in MenuRestaurant class only.
-        private double price;
+        //Array del menu y lo rellenamos con los platos que tendra el menu:
+        String[] dishes = {"Ensalada mixta", "Lasagna de verduras", "Hamburguesa con patatas", "Lubina al horno"};
 
-        // The menu variable is assigned in the constructor.
-        public MenuRestaurant(String menuName) {
-            menu = menuName;
+        ArrayList<String> menu = new ArrayList<>();
+        Collections.addAll(menu, dishes);
+
+        //Array de los precios y lo rellenamos con los precios de los platos que tendra el menu
+        int[] prices = {7, 8, 12, 15};
+
+        ArrayList<Integer> dishPrice = new ArrayList<>();
+        for (int menuPrice : prices) {
+            dishPrice.add(menuPrice);
         }
 
-        // The price variable is assigned a value.
-        public void setPrice(double menuPrice) {
-            price = menuPrice;
+        // Creo Iterator para mostrar en pantalla los platos ordenados por numero, con su precio.
+
+        int[] count = {1, 2, 3, 4};
+        ArrayList<Integer> dishCount = new ArrayList<>();
+        for (int menuCount : count) {
+            dishCount.add(menuCount);
         }
 
-        // This method prints the employee details.
-        public void printMenu() {
-            System.out.println("menu  : " + menu);
-            System.out.println("price :" + price);
+        Iterator<Integer> ITcount = dishCount.iterator();
+        Iterator<String> ITmenu = menu.iterator();
+        Iterator<Integer> ITprice = dishPrice.iterator();
+
+        while (ITmenu.hasNext()) {
+            System.out.println(ITcount.next() + " " + ITmenu.next() + " cuesta " + ITprice.next() + " €.");
         }
 
-        public static void main(String[] args) {
-            MenuRestaurant optionOne = new MenuRestaurant("Ensalada tibia de bacalao");
-            optionOne.setPrice(8.50);
-            optionOne.printMenu();
+        // Creamos un Array para guardar los precios de los platos escogidos
+        // Rellenamos el Array de pago de forma automática Bucle
 
-            MenuRestaurant optionTwo = new MenuRestaurant("Lasagna al horno");
-            optionTwo.setPrice(10.50);
-            optionTwo.printMenu();
+        ArrayList<Integer> order = new ArrayList<>();
+        ArrayList<Integer> pay = new ArrayList<>();
 
-            MenuRestaurant optionThree = new MenuRestaurant("Fideua");
-            optionThree.setPrice(15.50);
-            optionThree.printMenu();
+        int menuOrder;
+        System.out.println();
+        System.out.println("¿Desea algo para comer?, escriba el número del plato que desee escoger");
+        System.out.println("Pulse 0, si no desea ningún plato y quiere finalizar su pedido");
+
+        do {
+
+            Scanner input = new Scanner(System.in);
+            menuOrder = input.nextInt();
+            order.add(menuOrder);
+
+            if (menuOrder == 1) {
+                System.out.println("Ha escogido ensalada mixta y cuesta " + prices[0] + " €.");
+                System.out.println("¿Desea alguna otra cosa más?");
+                pay.add(7);
+            } else if (menuOrder == 2) {
+                System.out.println("Has escogido lasagna de verduras y cuesta " + prices[1] + " €.");
+                System.out.println("¿Desea alguna otra cosa más?");
+                pay.add(8);
+            } else if (menuOrder == 3) {
+                System.out.println("Has escogido hamburguesa con patatas y cuesta " + prices[2] + " €.");
+                System.out.println("¿Desea alguna otra cosa más?");
+                pay.add(12);
+            } else if (menuOrder == 4) {
+                System.out.println("Has escogido lubina al horno y cuesta " + prices[3] + " €.");
+                System.out.println("¿Desea alguna otra cosa más?");
+                pay.add(15);
+            } else System.out.println("Pedido completado.");
+            System.out.println();
+
+        } while (menuOrder != 0);
+
+        // Calculo del precio total del menú escogido.
+        int suma = 0;
+        for (Integer totalMenu : pay) {
+            suma = suma + totalMenu;
         }
+        System.out.println("El precio total de su pedido es de " + suma + " €.");
+        System.out.println();
+        System.out.println("Muchas gracias por su visita y buen provecho!");
     }
 }
